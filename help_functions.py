0ac00c339ae42e11e8902b00f5887f518b568006
Akademy <fukaiumi@yandex.ru>
2019-10-10 11:29:48 +0200

    nltk data

--- help_functions.py
+++ help_functions.py
@@ -9,6 +9,8 @@ import spacy
 #import textacy.extract
 import semistructured_statements
 
+nltk.data.path.append('nltk_data/')
+
 #download stop word list
 file = open("stopwords.txt", "r")
 stop_words = file.read().split()
